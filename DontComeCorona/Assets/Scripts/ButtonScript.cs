19bfa82284f76ebe07bcbd5f6a391dd4f23fe856
yamamoto <yoyoabab762@gmail.com>
2020-06-06 09:43:08 +0900

    initial commit

---
+++ DontComeCorona/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class ButtonScript : MonoBehaviour {
+ 
+    // ボタンが押された場合、今回呼び出される関数
+    public void OnClick()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ DontComeCorona/Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControllerScript : MonoBehaviour
+{
+
+    public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+    public float gravity = 10f;
+    public float jumpPower = 5;
+
+    private Vector3 moveDirection;
+    private CharacterController controller;
+
+    void Start()
+    {
+
+        // コンポーネントの取得
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    void Update()
+    {
+
+        if (controller.isGrounded)
+        {
+
+            // 回転
+            // transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+            // キャラクターのローカル空間での方向
+            moveDirection = transform.transform.forward * speed * Input.GetAxis("Vertical");
+
+            // ジャンプ
+            if (Input.GetButtonDown("Jump")) moveDirection.y = jumpPower;
+
+        }
+        else
+        {
+
+            // 重力
+            moveDirection.y -= gravity * Time.deltaTime;
+
+        }
+
+        // SimpleMove関数で移動させる
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+
+}
